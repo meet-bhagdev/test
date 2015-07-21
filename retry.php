55de4407d6d4276587cb05641f61275d8ec28ce3
Meet Bhagdev <mebha@microsoft.com>
2015-07-21 11:07:53 -0700

    Update retry.php

--- retry.php
+++ retry.php
@@ -9,7 +9,6 @@ $connectionOptions = array("Database"=>"AdventureWorks",
    "Uid"=>"yourusername", "PWD"=>"yourpassword", "LoginTimeout" => $connectionTimeoutSeconds);
 $conn;
 $errorArr = array();
-
 for ($cc = 1; $cc <= $maxCountTriesConnectAndQuery; $cc++)
 {
     $errorArr = array();
@@ -41,7 +40,6 @@ for ($cc = 1; $cc <= $maxCountTriesConnectAndQuery; $cc++)
     }
     // Adds any the error codes from the SQL Exception to an array.
     else {  
-
         if( ($errors = sqlsrv_errors() ) != null) {
             foreach( $errors as $error ) {
                 $errorArr[$ctr] = $error['code'];
@@ -49,19 +47,16 @@ for ($cc = 1; $cc <= $maxCountTriesConnectAndQuery; $cc++)
             }
         }
         $isTransientError = TRUE;
-
         // [A.4] Check whether sqlExc.Number is on the whitelist of transients.
         $isTransientError = IsTransientStatic($errorArr);
         if ($isTransientError == TRUE)  // Is a static persistent error...
         { 
             echo("Persistent error suffered, SqlException.Number==". $errorArr[0].". Program Will terminate."); 
             echo "<br>";
-
             // [A.5] Either the connection attempt or the query command attempt suffered a persistent SqlException.
             // Break the loop, let the hopeless program end.
             exit(0);
         }
-
         // [A.6] The SqlException identified a transient error from an attempt to issue a query command.
         // So let this method reloop and try again. However, we recommend that the new query
         // attempt should start at the beginning and establish a new connection.
@@ -69,22 +64,17 @@ for ($cc = 1; $cc <= $maxCountTriesConnectAndQuery; $cc++)
         {
             echo "Transient errors suffered in too many retries - " . $cc ." Program will terminate.";
             echo "<br>";
-
             exit(0);
         }
         echo("Transient error encountered.  SqlException.Number==". $errorArr[0]. " . Program might retry by itself.");  
         echo "<br>";
         echo $cc . " Attempts so far. Might retry.";
         echo "<br>";
-
-
         // A very simple retry strategy, a brief pause before looping. This could be changed to exponential if you want.
         sleep(1*$secondsBetweenRetries);
     }
     // [A.3] All has gone well, so let the program end.
-   
 }
-
 function IsTransientStatic($errorArr) {
     $arrayOfTransientErrorNumbers = array(4060, 10928, 10929, 40197, 40501, 40613);
     $result = array_intersect($arrayOfTransientErrorNumber, $errorArr);
